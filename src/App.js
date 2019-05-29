30e068a8dc652d1de8dda1375391ee3b5b418755
Shawn Hopkins <shopkins@seagen.com>
2019-05-28 18:55:58 -0700

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>goodbye</h1>
+          <h1>junk</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
